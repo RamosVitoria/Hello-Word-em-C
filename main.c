e270c418ded1fc04faf1e091db88e602a1e0f276
Vitória Emanuela Ramos <161209161+RamosVitoria@users.noreply.github.com>
2024-06-18 13:00:38 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,10 @@
+
+// Online C Compiler - Build, Compile and Run your C programs online in your favorite browser
+
+//#include<stdio.h>
+
+int main()
+{
+    printf("Hello world");
+    return 0;
+}
